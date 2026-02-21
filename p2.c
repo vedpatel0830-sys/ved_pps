42202c736b1402a879644e7628b4e39f9c417306
ved patel <vedpatel0940@gmail.com>
2026-02-21 10:19:12 +0530

    Update p2.c

--- p2.c
+++ p2.c
@@ -1,35 +1,4 @@
-#include <stdio.h>
 
-int main()
-{
-  int English,Maths,Electrical,PPS,FAI;
-  float per,Total;
-
-  printf("\nEnglish marks= ");
-  scanf("%d",&English);
-  printf("\nMaths marks= ");
-  scanf("%d",&Maths);
-  printf("\nElectrical marks= ");
-  scanf("%d",&Electrical);
-  printf("\nPPS marks= ");
-  scanf("%d",&PPS);
-  printf("\nFAI marks= ");
-  scanf("%d",&FAI);
-  Total=English+Maths+Electrical+PPS+FAI;
-  printf("\nTotal obtained mark out of 500 %f",Total);
-
-  per= Total/500*100;
-  printf("\nper = %f",per);
-  
-  if ( per>=90 )
-  {
-    printf("\nGrade=A");
-  }
-  else if ( per>=75 )
-  {
-    printf("\nGrade=B");
-  }
-  else if ( per>=60 )
   {
     printf("\nGrade=C");
   }
